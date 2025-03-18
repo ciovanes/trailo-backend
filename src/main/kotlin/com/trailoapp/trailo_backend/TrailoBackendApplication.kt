42dcc9352d274011100addd4853a6ef751bcde42
ciovanes <iovanescuclaudiu@proton.me>
2025-03-18 22:34:30 +0100

    Initial commit

---
+++ src/main/kotlin/com/trailoapp/trailo_backend/TrailoBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.trailoapp.trailo_backend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TrailoBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<TrailoBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/trailoapp/trailo_backend/TrailoBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.trailoapp.trailo_backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TrailoBackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
