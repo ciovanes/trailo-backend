60cfdd63d3f8695e99b0b56969f6a7461c39fd37
ciovanes <iovanescuclaudiu@proton.me>
2025-04-25 22:27:57 +0200

    feat(db): modify database schema for user entity

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -1,13 +1,17 @@
-CREATE DATABASE IF NOT EXISTS trailo;
-
 CREATE SCHEMA IF NOT EXISTS core;
 
 CREATE TABLE IF NOT EXISTS core."user"(
     uuid UUID NOT NULL DEFAULT gen_random_uuid(),
     email text NOT NULL,
     username text NOT NULL,
+    cognito_id text NOT NULL,
+    name text,
+    surname text,
+    profile_picture text,
+    country text,
     created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT user_pk PRIMARY KEY (uuid),
     CONSTRAINT uk_email UNIQUE (email),
-    CONSTRAINT uk_username UNIQUE (username)
+    CONSTRAINT uk_username UNIQUE (username),
+    CONSTRAINT uk_cognito_id UNIQUE (cognito_id)
 );
