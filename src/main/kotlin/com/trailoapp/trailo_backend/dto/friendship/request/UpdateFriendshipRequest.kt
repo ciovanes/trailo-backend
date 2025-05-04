66f323546c1963095f590ca4ed9bce92bb11633b
ciovanes <iovanescuclaudiu@proton.me>
2025-05-04 21:19:03 +0200

    fix(dto): add message field to NotNull annotation

--- src/main/kotlin/com/trailoapp/trailo_backend/dto/friendship/request/UpdateFriendshipRequest.kt
+++ src/main/kotlin/com/trailoapp/trailo_backend/dto/friendship/request/UpdateFriendshipRequest.kt
@@ -5,7 +5,7 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Pattern
 
 data class UpdateFriendshipRequest (
-    @field:NotNull("Status is required")
+    @field:NotNull(message = "Status is required")
     @field:Pattern(regexp = "ACCEPTED|PENDING|REJECTED", message = "Status must be ACCEPTED, PENDING or REJECTED")
     val status: String
 )
