d3253f00e3d22d853c1b8cd817e85bad48db0fc3
ciovanes <iovanescuclaudiu@proton.me>
2025-05-06 23:50:58 +0200

    feat(db): add group and user_group tables; status and roles enum

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -39,3 +39,47 @@ CREATE TABLE IF NOT EXISTS social.friendship(
 CREATE INDEX idx_friendship_user_id ON social.friendship(user_id);
 CREATE INDEX idx_friendship_friend_id ON social.friendship(friend_id);
 CREATE INDEX idx_friendship_status ON social.friendship(status);
+
+-- Groups
+CREATE TYPE social.membership_status AS ENUM(
+    'PENDING',
+    'ACCEPTED',
+    'REJECTED'
+);
+
+CREATE TYPE social.group_role AS ENUM(
+    'LEADER',
+    'CO_LEADER',
+    'ELDER',
+    'MEMBER'
+);
+
+CREATE TABLE IF NOT EXISTS social.group(
+    uuid UUID NOT NULL DEFAULT gen_random_uuid(),
+    name text NOT NULL,
+    description text,
+    private boolean NOT NULL DEFAULT false,
+    image_url text,
+    last_modified_at timestamptz,
+    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT group_pk PRIMARY KEY (uuid),
+    CONSTRAINT uk_name UNIQUE (name)
+);
+
+CREATE INDEX idx_group_name ON social.group(name);
+
+CREATE TABLE IF NOT EXISTS social.user_group(
+    uuid UUID NOT NULL DEFAULT gen_random_uuid(),
+    group_id UUID NOT NULL REFERENCES social.group(uuid) ON DELETE CASCADE,
+    user_id UUID NOT NULL REFERENCES core."user"(uuid) ON DELETE CASCADE,
+    status social.membership_status NOT NULL,
+    role social.group_role NOT NULL DEFAULT 'MEMBER',
+    invited_by UUID NOT NULL,
+    is_favorite boolean DEFAULT false,
+    last_modified_at timestamptz,
+    join_date timestamptz DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT user_group_pk PRIMARY KEY (uuid),
+    CONSTRAINT uk_group_user UNIQUE (group_id, user_id)
+);
