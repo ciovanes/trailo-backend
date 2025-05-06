9bbd448167c608c7a9584a98904ca67a17dfc468
ciovanes <iovanescuclaudiu@proton.me>
2025-05-06 23:55:17 +0200

    feat(domain): add group-related entities

---
+++ src/main/kotlin/com/trailoapp/trailo_backend/domain/enum/GroupRoles.kt
@@ -0,0 +1,5 @@
+package com.trailoapp.trailo_backend.domain.enum
+
+enum class GroupRoles {
+    LEADER, CO_LEADER, ELDER, MEMBER
+}
---
+++ src/main/kotlin/com/trailoapp/trailo_backend/domain/enum/MembershipStatus.kt
@@ -0,0 +1,5 @@
+package com.trailoapp.trailo_backend.domain.enum
+
+enum class MembershipStatus {
+    PENDING, ACCEPTED, REJECTED
+}
