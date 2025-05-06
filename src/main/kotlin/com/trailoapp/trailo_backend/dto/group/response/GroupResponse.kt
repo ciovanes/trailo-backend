1878d002d19d08624d0eac0c340a9bde1dd0e25c
ciovanes <iovanescuclaudiu@proton.me>
2025-05-07 00:04:18 +0200

    feat(dto): add Group and UserGroup DTOs

---
+++ src/main/kotlin/com/trailoapp/trailo_backend/dto/group/response/GroupResponse.kt
@@ -0,0 +1,28 @@
+package com.trailoapp.trailo_backend.dto.group.response
+
+import com.trailoapp.trailo_backend.domain.social.GroupEntity
+import java.util.UUID
+
+data class GroupResponse(
+    val uuid: UUID,
+    val name: String,
+    val description: String? = null,
+    val isPrivate: Boolean,
+    val imageUrl: String? = null,
+    val lastModifiedAt: String? = null,
+    val createdAt: String?,
+) {
+    companion object {
+        fun fromEntity(group: GroupEntity): GroupResponse {
+            return GroupResponse(
+                uuid = group.uuid,
+                name = group.name,
+                description = group.description,
+                isPrivate = group.isPrivate,
+                imageUrl = group.imageUrl,
+                lastModifiedAt = group.lastModifiedAt?.toString(),
+                createdAt = group.createdAt.toString()
+            )
+        }
+    }
+}
