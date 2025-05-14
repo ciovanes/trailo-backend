196f1af8f84cda874e604db2829a6a0e34877620
ciovanes <iovanescuclaudiu@proton.me>
2025-05-14 17:18:40 +0200

    feat(exception): add HttpRequestMethodNotSupportedException handler

--- src/main/kotlin/com/trailoapp/trailo_backend/exception/handler/GlobalExceptionHandler.kt
+++ src/main/kotlin/com/trailoapp/trailo_backend/exception/handler/GlobalExceptionHandler.kt
@@ -12,6 +12,7 @@ import org.slf4j.LoggerFactory
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.http.converter.HttpMessageNotReadableException
+import org.springframework.web.HttpRequestMethodNotSupportedException
 import org.springframework.web.bind.MethodArgumentNotValidException
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -84,6 +85,10 @@ class GlobalExceptionHandler {
     @ExceptionHandler(NotAuthorizedException::class)
     fun handleNotAuthorizedException(ex: NotAuthorizedException) =
         buildErrorResponse(HttpStatus.UNAUTHORIZED, "Not authorized", ex.message)
+
+    @ExceptionHandler(HttpRequestMethodNotSupportedException::class)
+    fun handleHttpRequestMethodNotSupportedException(ex: HttpRequestMethodNotSupportedException) =
+        buildErrorResponse(HttpStatus.METHOD_NOT_ALLOWED, "Method not allowed", ex.message)
 
     // ===== RESOURCE EXCEPTIONS =====
 
