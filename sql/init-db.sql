169fc90f493e86dc01973ffa2671e2403a614c8a
ciovanes <iovanescuclaudiu@proton.me>
2025-05-17 00:43:30 +0200

    refactor(sql): improve SQL syntax and move group table to core schema

--- sql/init-db.sql
+++ sql/init-db.sql
@@ -1,85 +1,83 @@
+-- schemas
 CREATE SCHEMA IF NOT EXISTS core;
 CREATE SCHEMA IF NOT EXISTS social;
 
+-- enums
+CREATE TYPE social.friendship_status AS ENUM('PENDING', 'ACCEPTED', 'REJECTED');
+CREATE TYPE social.membership_status AS ENUM('PENDING', 'ACCEPTED', 'REJECTED');
+CREATE TYPE social.group_role AS ENUM('LEADER', 'CO_LEADER', 'ELDER', 'MEMBER');
+
+-- core
 CREATE TABLE IF NOT EXISTS core."user"(
-    uuid UUID NOT NULL DEFAULT gen_random_uuid(),
-    email text NOT NULL,
-    username text NOT NULL,
-    cognito_id text NOT NULL,
-    name text,
-    surname text,
-    profile_picture text,
-    country text,
-    last_login_at timestamptz,
-    last_modified_at timestamptz DEFAULT CURRENT_TIMESTAMP,
-    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+    uuid UUID NOT NULL,
+    email TEXT NOT NULL,
+    username TEXT NOT NULL,
+    cognito_id TEXT NOT NULL,
+    name TEXT,
+    surname TEXT,
+    profile_picture TEXT,
+    country TEXT,
+
+    last_login_at TIMESTAMPTZ,
+    last_modified_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+
     CONSTRAINT user_pk PRIMARY KEY (uuid),
     CONSTRAINT uk_email UNIQUE (email),
     CONSTRAINT uk_username UNIQUE (username),
     CONSTRAINT uk_cognito_id UNIQUE (cognito_id)
-);
+    );
 
-CREATE TYPE social.friendship_status AS ENUM(
-    'PENDING',
-    'ACCEPTED',
-    'REJECTED'
-);
+CREATE TABLE IF NOT EXISTS core.group(
+    uuid UUID NOT NULL,
+    name TEXT NOT NULL,
+    description TEXT,
+    private BOOLEAN NOT NULL DEFAULT false,
+    image_url TEXT,
 
+    last_modified_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
+
+    CONSTRAINT group_pk PRIMARY KEY (uuid),
+    CONSTRAINT uk_name UNIQUE (name)
+    );
+
+CREATE INDEX idx_group_name ON core.group(name);
+
+-- social
 CREATE TABLE IF NOT EXISTS social.friendship(
-    uuid UUID NOT NULL DEFAULT gen_random_uuid(),
+    uuid UUID NOT NULL,
     user_id UUID NOT NULL REFERENCES core."user"(uuid) ON DELETE CASCADE,
     friend_id UUID NOT NULL REFERENCES core."user"(uuid) ON DELETE CASCADE,
     status social.friendship_status NOT NULL,
-    last_modified_at timestamptz,
-    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
+
+    last_modified_at TIMESTAMPTZ NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL,
 
     CONSTRAINT uq_friendship_user_friend UNIQUE(user_id, friend_id)
-);
+    );
 
 CREATE INDEX idx_friendship_user_id ON social.friendship(user_id);
 CREATE INDEX idx_friendship_friend_id ON social.friendship(friend_id);
 CREATE INDEX idx_friendship_status ON social.friendship(status);
 
--- Groups
-CREATE TYPE social.membership_status AS ENUM(
-    'PENDING',
-    'ACCEPTED',
-    'REJECTED'
-);
-
-CREATE TYPE social.group_role AS ENUM(
-    'LEADER',
-    'CO_LEADER',
-    'ELDER',
-    'MEMBER'
-);
-
-CREATE TABLE IF NOT EXISTS social.group(
-    uuid UUID NOT NULL DEFAULT gen_random_uuid(),
-    name text NOT NULL,
-    description text,
-    private boolean NOT NULL DEFAULT false,
-    image_url text,
-    last_modified_at timestamptz,
-    created_at timestamptz DEFAULT CURRENT_TIMESTAMP,
-
-    CONSTRAINT group_pk PRIMARY KEY (uuid),
-    CONSTRAINT uk_name UNIQUE (name)
-);
-
-CREATE INDEX idx_group_name ON social.group(name);
 
 CREATE TABLE IF NOT EXISTS social.user_group(
-    uuid UUID NOT NULL DEFAULT gen_random_uuid(),
-    group_id UUID NOT NULL REFERENCES social.group(uuid) ON DELETE CASCADE,
+    uuid UUID NOT NULL,
+    group_id UUID NOT NULL REFERENCES core.group(uuid) ON DELETE CASCADE,
     user_id UUID NOT NULL REFERENCES core."user"(uuid) ON DELETE CASCADE,
     status social.membership_status NOT NULL,
     role social.group_role NOT NULL DEFAULT 'MEMBER',
     invited_by UUID NOT NULL,
-    is_favorite boolean DEFAULT false,
-    last_modified_at timestamptz,
-    join_date timestamptz DEFAULT CURRENT_TIMESTAMP,
+    is_favorite BOOLEAN DEFAULT false,
+
+    last_modified_at TIMESTAMPTZ NOT NULL,
+    join_date TIMESTAMPTZ NOT NULL,
 
     CONSTRAINT user_group_pk PRIMARY KEY (uuid),
     CONSTRAINT uk_group_user UNIQUE (group_id, user_id)
-);
+    );
+
+CREATE INDEX idx_user_group_group_id ON social.user_group(group_id);
+CREATE INDEX idx_user_group_user_id ON social.user_group(user_id);
+CREATE INDEX idx_user_group_status ON social.user_group(status);
