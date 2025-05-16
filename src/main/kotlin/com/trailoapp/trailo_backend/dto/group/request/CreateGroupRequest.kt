d20da47e6431c3fba61b4a0401058a4a1fff1a48
ciovanes <iovanescuclaudiu@proton.me>
2025-05-16 20:52:34 +0200

    refactor(dto): add validation constraints to dto's

--- src/main/kotlin/com/trailoapp/trailo_backend/dto/group/request/CreateGroupRequest.kt
+++ src/main/kotlin/com/trailoapp/trailo_backend/dto/group/request/CreateGroupRequest.kt
@@ -5,7 +5,11 @@ import jakarta.validation.constraints.NotNull
 data class CreateGroupRequest(
     @field:NotNull(message = "Group name is required")
     val name: String,
+
     val description: String? = null,
+
+    @field:NotNull(message = "Group privacy is required")
     val isPrivate: Boolean,
+
     val imageUrl: String? = null,
 )
